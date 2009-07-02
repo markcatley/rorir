8440abeecd44917cabdb44310137307cf4cade7c
Mark Catley <mark@nexx.co.nz>
2009-07-02 16:26:09 +1200

    Limiting routes to get requests.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 ActionController::Routing::Routes.draw do |map|
-  map.rorir 'rorir/:style/:text.:format', :controller => 'rorir', :action => 'show'
+  map.rorir 'rorir/:style/:text.:format', :controller => 'rorir', :action => 'show', :conditions => {:method => :get}
 end
